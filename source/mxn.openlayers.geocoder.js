700699fc075223106f3eda90ed29c2123eda2e08
Giles Collingwood <giles@earthware.co.uk>
2012-12-23 15:46:35 +0000

    added optional parameter to request multiple results from the geocoder api, and implemented it in open layers and the corresponding example. Existing api that returns just 1 result is uneffected, so this is a non-breaking change.

--- source/mxn.openlayers.geocoder.js
+++ source/mxn.openlayers.geocoder.js
@@ -6,9 +6,9 @@ Geocoder: {
 		var me = this;
 	},
 	
-	geocode: function(address){
+	geocode: function(address, rowlimit){
 		var me = this;
-		
+		rowlimit = rowlimit || 1; //default to one result 
 		if (!address.hasOwnProperty('address') || address.address === null || address.address === '') {
 			address.address = [ address.street, address.locality, address.region, address.country ].join(', ');
 		}
@@ -23,7 +23,7 @@ Geocoder: {
 					'addressdetails': 1,
 					'format': 'json'
 				},
-				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status); }
+				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status, rowlimit); }
 			});
 		} else {
 			OpenLayers.Request.GET({
@@ -33,62 +33,65 @@ Geocoder: {
 					'addressdetails': 1,
 					'format': 'json'
 				},
-				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status); }
+				callback: function(request) { me.geocode_callback(JSON.parse(request.responseText), request.status, rowlimit); }
 			});
 		}
 	},
 	
-	geocode_callback: function(results, status){
-		var return_location = {};
-		
+	geocode_callback: function(results, status, rowlimit){
 		if (status != 200) {
 			this.error_callback(response.statusText);
 		}
 		else if (results instanceof Array && !results.length){
 			this.error_callback("OpenLayers didn't recognize this address.");
 		}
-		else {
-			return_location.street = '';
-			return_location.locality = '';
-			return_location.postcode = '';
-			return_location.region = '';
-			return_location.country = '';
-			
+		else {		
 			var place;
+			var places = [];
 			
-			if (results.length > 0) {
-				place = results[0];
-			} else {
-				place = results;
+			for (i=0; i<results.length; i++)
+			{
+				place = results[i];
+				var return_location = {};
+				return_location.street = '';
+				return_location.locality = '';
+				return_location.postcode = '';
+				return_location.region = '';
+				return_location.country = '';
+				var street_components = [];
+				
+				if (place.address.country) {
+					return_location.country = place.address.country;
+				}
+				if (place.address.state) {
+					return_location.region = place.address.state;
+				}
+				if (place.address.city) {
+					return_location.locality = place.address.city;
+				}
+				if (place.address.postcode) {
+					return_location.postcode = place.address.postcode;
+				}
+				if (place.address.road) {
+					street_components.push(place.address.road);
+				}
+				if (place.address.house_number) {
+					street_components.unshift(place.address.house_number);
+				}
+				
+				if (return_location.street === '' && street_components.length > 0) {
+					return_location.street = street_components.join(' ');
+				}
+				
+				return_location.point = new mxn.LatLonPoint(parseFloat(place.lat), parseFloat(place.lon));
+				
+				places.push(return_location);
 			}
-			var street_components = [];
 			
-			if (place.address.country) {
-				return_location.country = place.address.country;
-			}
-			if (place.address.state) {
-				return_location.region = place.address.state;
-			}
-			if (place.address.city) {
-				return_location.locality = place.address.city;
-			}
-			if (place.address.postcode) {
-				return_location.postcode = place.address.postcode;
-			}
-			if (place.address.road) {
-				street_components.push(place.address.road);
-			}
-			if (place.address.house_number) {
-				street_components.unshift(place.address.house_number);
-			}
-			
-			if (return_location.street === '' && street_components.length > 0) {
-				return_location.street = street_components.join(' ');
-			}
-			
-			return_location.point = new mxn.LatLonPoint(parseFloat(place.lat), parseFloat(place.lon));
-			
-			this.callback(return_location);
+			if (rowlimit <= 1)
+				{this.callback(places[0]);}
+			else
+				{this.callback(places.slice(0,rowlimit));}
 		}
 	}
 }
